d1c54463abbd82928e0f67ddd7e6a32095bf87c9
sbstjn <mail@sbstjn.com>
2015-10-17 16:43:31 +0200

    Removed Traits usage. So PHP >=5.3 works

--- load.php
+++ load.php
@@ -2,8 +2,6 @@
 
 require_once __DIR__ . '/config.php';
 
-require_once __DIR__ . '/src/traits/character.php';
-
 require_once __DIR__ . '/src/lib/Release.php';
 
 require_once __DIR__ . '/src/lib/Component/Base.php';
